265d6ddcbec5a9d60720c098b037449e44f833fa
Johnny-MetaTraderSolutions <xiaoqiqu.mt@gmail.com>
2022-03-30 10:31:52 +1100

    Finished example of some basic stylesheet practice.

--- rn-starter/src/screens/BoxLayout.js
+++ rn-starter/src/screens/BoxLayout.js
@@ -3,21 +3,45 @@ import { View,Text,StyleSheet } from 'react-native'
 
 const BoxLayout= ()=>{
     return <View style = {styles.viewStyle}>
-        <Text style ={styles.textStyle}>Child #1</Text>
-        <Text style ={styles.textStyle}>Child #2</Text>
-        <Text style ={styles.textStyle}>Child #3</Text>
+        <Text style ={styles.textOneStyle}>Child #1</Text>
+        <Text style ={styles.textTwoStyle}>Child #2</Text>
+        <Text style ={styles.textThreeStyle}>Child #3</Text>
     </View>
 }
 
 const styles = StyleSheet.create({
     viewStyle:{
+        //alignItems, flexDirection are always add on the parent elements
+        alignItems:'stretch',
         borderWidth:5,
+        height:100,
         borderColor: "black",
+        flexDirection:"row",
+        justifyContent:"space-around"
     },
-    textStyle:{
+    textOneStyle:{
         borderWidth:5,
         borderColor: "red",
-    }
+        flex: 4
+    },
+    textTwoStyle:{
+        borderWidth:5,
+        borderColor: "red",
+        flex: 3,
+        // the next line of code is equivalent to the following:
+        // position:"absolute",
+        // top:0,
+        // bottom:0,
+        // left:0,
+        // right:0,
+        ...StyleSheet.absoluteFillObject,
+    },
+    textThreeStyle:{
+        borderWidth:5,
+        borderColor: "red",
+        flex: 1,
+        alignSelf: "center"
+    },
 
 })
 
