fd34ef37e9ef3a5fa317e83834f071592383183a
Johnny-MetaTraderSolutions <xiaoqiqu.mt@gmail.com>
2022-03-30 12:17:08 +1100

    initial setup for new project as an example of API usage

---
+++ food/src/screens/SearchScreen.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View,Text,StyleSheet } from 'react-native'
+
+const SearchScreen= ()=>{
+    return <View><Text>search Screen</Text></View>
+}
+
+const styles = StyleSheet.create({})
+
+export default SearchScreen
