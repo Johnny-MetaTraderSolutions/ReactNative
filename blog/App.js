62d0094d84cf4ebb90180e0f247362bd1a00faf1
Johnny-MetaTraderSolutions <xiaoqiqu.mt@gmail.com>
2022-04-04 17:21:57 +1000

    update of blog project

--- blog/App.js
+++ blog/App.js
@@ -1,17 +1,28 @@
+import React from "react";
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import IndexScreen from "./src/screens/IndexScreen";
+import ShowScreen from "./src/screens/ShowScreen";
+import CreateScreen from "./src/screens/CreateScreen";
+import EditScreen from "./src/screens/EditScreen";
 
 const navigator = createStackNavigator(
     {
         Index: IndexScreen,
+        Show: ShowScreen,
+        Edit: EditScreen,
+        Create: CreateScreen,
     },
     {
         initialRouteName: "Index",
         defaultNavigationOptions: {
-            title: "Blog",
+            title: "Blog List",
         },
     }
 );
 
-export default createAppContainer(navigator);
+const App = createAppContainer(navigator);
+
+export default ()=>{
+    return <App/>
+}
---
+++ blog/src/screens/EditScreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { View,Text,StyleSheet } from 'react-native'
+
+const EditScreen= ()=>{
+    return <Text>this is the main page</Text>
+
+}
+
+const styles = StyleSheet.create({})
+
+export default EditScreen
